5c8a7f095c5ee8dbc55d14c4253e0094610bb839
bananaship <1084756833@qq.com>
2018-04-07 20:44:42 +0800

    新增小注释，可忽略

--- ALM_RPCA/inexact_alm_rpca/mydemo.m
+++ ALM_RPCA/inexact_alm_rpca/mydemo.m
@@ -1,4 +1,4 @@
-D = rand(100,100);
+D = rand(100,100); %产生100*100的随机矩阵
 [A_hat,E_hat,iter] = inexact_alm_rpca(D);
 disp(['A的秩为：',num2str(rank(A_hat)),'/',num2str(size(D,1))]);
 disp(['E中零元素个数为',num2str(length(find(E_hat==0))),'/',num2str(size(D,1)*size(D,2))]);
